0c43a166093e16013825fc58919b4dc1f530b288
curtis <curtis@curtis-laptop.(none)>
2013-01-13 20:45:13 +0800

    Add simple logger for Crawlite4j core

---
+++ src/core/cn/crawlite4j/log/Level.java
@@ -0,0 +1,12 @@
+package cn.crawlite4j.log;
+
+public class Level {
+	public static final int OFF = 0;
+	public static final int FATAL = 1;
+	public static final int ERROR = 4;
+	public static final int WARN = 5;
+	public static final int INFO = 7;
+	public static final int DEBUG = 8;
+	public static final int TRACE = 9;
+	public static final int ALL = 10;
+}
